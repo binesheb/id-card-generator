e7a8a7dec90064277dba2f42b252e2bc9f6a682e
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-07 15:26:42 +0530

    Expose secure updater controls to renderer

--- preload.js
+++ preload.js
@@ -2,5 +2,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('idCardDesktop', {
   chooseOutputDirectory: () => ipcRenderer.invoke('choose-output-directory'),
-  saveJpgs: (payload) => ipcRenderer.invoke('save-jpgs', payload)
+  saveJpgs: (payload) => ipcRenderer.invoke('save-jpgs', payload),
+  checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+  downloadUpdate: () => ipcRenderer.invoke('download-update'),
+  installUpdate: () => ipcRenderer.invoke('install-update'),
+  onUpdateStatus: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('update-status', listener);
+    return () => ipcRenderer.removeListener('update-status', listener);
+  }
 });
